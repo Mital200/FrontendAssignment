0186849a139d0d30dd82ac400b21cb12f068beaf
mital200 <raghuvanshi.mital@webseeder.tech>
2025-07-29 18:03:56 +0530

    updating

--- websitepage/src/components/Navbar.jsx
+++ websitepage/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-4 sm:px-8 py-4 bg-white shadow flex-col sm:flex-row">
-      <h1 className="text-2xl font-bold text-green-800 mb-2 sm:mb-0">Fresh<br/>Foods</h1>
-      <ul className="flex flex-wrap justify-center gap-4 text-gray-700 text-sm sm:text-base">
+      <h1 className="text-2xl font-bold text-green-800 mb-2 sm:mb-0"><img src = "./freshfood.avif " className="w-40 h-40 object-contain"></img></h1>
+      <ul className="flex flex-wrap justify-center gap-4 text-gray-700 text-lg ">
         <li>Home</li>
         <li>Categories</li>
         <li>Pages</li>
